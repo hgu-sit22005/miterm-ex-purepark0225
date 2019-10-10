58e4431b88dc84e1220fc3d0fa306cd10cf0443c
purepark0225 <21300319@handong.edu>
2019-10-10 15:08:56 +0900

    problem01

--- Problem01/Problem01.cpp
+++ Problem01/Problem01.cpp
@@ -4,19 +4,20 @@
 
 char* generate_3_digit()
 {
+	char* res = new char[10];
 
+	for(int i = 0; i < 10; i++)
+		res[i] = i + '0';
 
-
-
-
-
-
-
-
-
-
-
-
+	for(int i = 0; i < 3; i++)
+	{
+		chr bufl
+		int index = rand()%(10-i);
+		buf = res[index];
+		res[i] = res[index];
+		res[index] = buf;
+	}
+	return res;
 }
 
 int main()
